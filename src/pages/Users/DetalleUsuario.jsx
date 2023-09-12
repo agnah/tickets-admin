a2960bdb3be136b18b5c6b525acd7bdf8a659446
= <=>
2023-09-12 20:02:15 -0300

    Pantalla Detalle Usuario v0.0.1

--- src/pages/Users/DetalleUsuario.jsx
+++ src/pages/Users/DetalleUsuario.jsx
@@ -1,11 +1,13 @@
 import useAuth from '@servicios/UseAuth'
+import Tablero from '@components/Tablero/Tablero'
+import UserDetail from '../../components/partials/Users/UserDetail'
 
 function Profile () {
   const { user } = useAuth()
   return (
-    <div>
-      Sector: {user?.sector} Perfil: {user?.perfil} Rol: {user?.rolUsuario} Nombre: {user?.nombre}
-    </div>
+    <Tablero title="Detalle usuario" page="Usuarios / Detalle usuario">
+      <UserDetail user={user} />
+    </Tablero>
   )
 }
 
