c08158aee1e827a633684be009c55e2eb5df7e57
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2023-10-08 19:08:32 -0300

    Modificacion endpoint usuarios

--- src/constantes/constApis.js
+++ src/constantes/constApis.js
@@ -4,7 +4,7 @@ export const apis = {
   API_TICKETS_DETALLE: '../../assets/ticket.json',
   // API_TICKETS: 'https://my.api.mockaroo.com/ticket.json?key=d2c25b50',
   // API_TICKETS: 'https://dummyjson.com/users?limit=100',
-  API_USUARIOS: 'http://localhost:8000/api/usuario/list/',
+  API_USUARIOS: 'http://localhost:8000/api/usuario/all/',
   API_USUARIO: 'http://localhost:8000/api/usuario/?field=id&value='
   // API_USUARIO: 'https://my.api.mockaroo.com/ticket/{id}.json?key=d2c25b50'
 }
